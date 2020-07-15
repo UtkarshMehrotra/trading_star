00e90d4758c1e43bc6fb66039b5010cfc110ef12
UtkarshMehrotra <umehrotra97@gmail.com>
2020-07-15 09:46:28 +0530

    Create distance.js
    Calculate distance between 2 cities.

---
+++ distance.js
@@ -0,0 +1,30 @@
+var distance = require('google-distance-matrix');
+
+var origins = ['San Francisco CA'];
+var destinations = ['New York NY'];
+
+distance.key('AIzaSyBak6jPtlw0-LEwns1UnqnCNAuJo0hUBjU');
+distance.units('imperial');
+
+distance.matrix(origins, destinations, function (err, distances) {
+    if (err) {
+        return console.log(err);
+    }
+    if(!distances) {
+        return console.log('no distances');
+    }
+    if (distances.status == 'OK') {
+        for (var i=0; i < origins.length; i++) {
+            for (var j = 0; j < destinations.length; j++) {
+                var origin = distances.origin_addresses[i];
+                var destination = distances.destination_addresses[j];
+                if (distances.rows[0].elements[j].status == 'OK') {
+                    var distance = distances.rows[i].elements[j].distance.text;
+                    console.log('Distance from ' + origin + ' to ' + destination + ' is ' + distance);
+                } else {
+                    console.log(destination + ' is not reachable by land from ' + origin);
+                }
+            }
+        }
+    }
+});
